64358b6650c7b55c8f74d5fdd1bba86c009ce36b
Mateo071 <floresmateo2000@gmail.com>
2024-12-09 10:47:05 -0500

    added jump feature and changed clear method

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def move_backward():
   tim.back(10)
 
 def turn_right():
-  tim.right(18)
+  tim.right(10)
 
 def turn_left():
-  tim.left(18)
+  tim.left(10)
+
+def clear():
+  tim.reset()
+
+def jump():
+  if tim.isdown():
+    tim.up()
+  else:tim.down()
 
 screen.listen()
 
@@ -21,4 +29,6 @@ screen.onkey(move_forward, "Up")
 screen.onkey(move_backward, "Down")
 screen.onkey(turn_left, "Left")
 screen.onkey(turn_right, "Right")
+screen.onkey(clear, "c")
+screen.onkey(jump, "space")
 screen.exitonclick()
